cd2db8a67f77353518c2fcf3f0de9e98eacb9479
CodyGramlich <32050152+CodyGramlich@users.noreply.github.com>
2017-11-19 01:09:32 -0700

    Added a random route.

--- server.py
+++ server.py
@@ -1,17 +1,19 @@
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
+
+@app.route('/joke/random')
+def Punch():
+    return '{"text": "this is a joke"}'
+
 @app.route('/')
 def display():
     return render_template("index.html")
 
-@app.route('/Joke')
+@app.route('/joke')
 def Joke():
     return '<h2> What did 0 say to 8 <h2/>'
 
-@app.route('/Joke/Punch')
-def Punch():
-    return '<h2> Nice Belt <h2/>'
 
 if __name__== '__main__':
     app.run(debug = True)
