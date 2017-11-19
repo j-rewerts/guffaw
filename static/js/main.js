7598da52375527a7b72b7c44ec2923efb1a9158c
CodyGramlich <32050152+CodyGramlich@users.noreply.github.com>
2017-11-19 01:09:08 -0700

    Added paragraph writing.

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,19 @@
 function reqListener () {
   console.log(this.responseText);
+  var name = JSON.parse(this.responseText);
+  joke.innerHTML=name.text;
+
 }
 
 var button = document.getElementById("button");
+
+var joke = document.getElementById("joke");
 
 button.addEventListener("click", function() {
   console.log("Button clicked");
 
   var oReq = new XMLHttpRequest();
   oReq.addEventListener("load", reqListener);
-  oReq.open("GET", "http://localhost:5000");
+  oReq.open("GET", "http://localhost:5000/joke/random");
   oReq.send();
 });
